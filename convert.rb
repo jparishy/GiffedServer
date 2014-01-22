059b50e2d92039e31d1668b1b28cf47a825ead4e
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2014-01-21 23:13:27 -0500

    Optimize GIF post creation

--- convert.rb
+++ convert.rb
@@ -2,10 +2,15 @@
 
 require 'tmpdir'
 
+# Get the frames
 out_filename_base = "/tmp/" + Dir::Tmpname.make_tmpname('converted', nil)
 `ffmpeg -i '#{ARGV[0]}' -s 240x320 -vf "transpose=1" #{out_filename_base}_%05d.gif`
 
+# Make the frames a GIF
 output_filename = "/tmp/" + Dir::Tmpname.make_tmpname('output', nil) + ".gif"
 `gifsicle --loop #{out_filename_base}* > #{output_filename}`
+
+# Optimize the GIF
+`convert #{output_filename} -coalesce -layers optimize #{output_filename}`
 
 puts output_filename
