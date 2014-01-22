5f89a80c6fb825e26824082638438065c387bb87
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2014-01-21 23:40:51 -0500

    Reduce number of colors if needed

--- convert.rb
+++ convert.rb
@@ -6,11 +6,15 @@ require 'tmpdir'
 out_filename_base = "/tmp/" + Dir::Tmpname.make_tmpname('converted', nil)
 `ffmpeg -i '#{ARGV[0]}' -s 240x320 -vf "transpose=1" #{out_filename_base}_%05d.gif`
 
-# Make the frames a GIF
+# Make the frames a GIF and optimize it, reducing number of colors until <2MB
 output_filename = "/tmp/" + Dir::Tmpname.make_tmpname('output', nil) + ".gif"
-`gifsicle --loop #{out_filename_base}* > #{output_filename}`
 
-# Optimize the GIF
-`convert #{output_filename} -coalesce -layers optimize #{output_filename}`
+num_colors = 128
+while true do
+    `gifsicle -O3 --colors #{num_colors} --loop #{out_filename_base}* > #{output_filename}`
+    num_colors = num_colors == 128 ? 64 : num_colors - 8
+    if File.stat(output_filename).size <= 2 * 10 ** 6 || num_colors < 24
+        break
+end
 
 puts output_filename
