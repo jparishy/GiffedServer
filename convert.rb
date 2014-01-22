2684867a2558c500f491b3385a13e87ad5549ac2
Julius Parishy <julius@fitocracy.com>
2014-01-22 00:20:07 -0500

    changes i think?

--- convert.rb
+++ convert.rb
@@ -11,10 +11,11 @@ output_filename = "/tmp/" + Dir::Tmpname.make_tmpname('output', nil) + ".gif"
 
 num_colors = 128
 while true do
-    `gifsicle -O3 --colors #{num_colors} --loop #{out_filename_base}* > #{output_filename}`
-    num_colors = num_colors == 128 ? 64 : num_colors - 8
-    if File.stat(output_filename).size <= 2 * 10 ** 6 || num_colors < 24
-        break
+  `gifsicle -O3 --colors #{num_colors} --loop #{out_filename_base}* > #{output_filename}`
+  num_colors = num_colors == 128 ? 64 : num_colors - 8
+  if File.stat(output_filename).size <= 2 * 10 ** 6 || num_colors < 24
+      break
+  end
 end
 
 puts output_filename
